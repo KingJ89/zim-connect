ef1f7895c83a4d27ab4d09cdf983fe4b91594abc
KingJ89 <janmutewera@gmail.com>
2025-01-29 22:40:39 +0200

    Task shop.html updated

---
+++ ZimConnect/public/registerForm.jsx
@@ -0,0 +1,28 @@
+// RegisterForm.jsx
+import { useState } from 'react';
+import axios from 'axios';
+
+const RegisterForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/api/auth/register', { email, password });
+      alert(res.data.message);
+    } catch (err) {
+      alert('Error: ' + err.response.data.errors[0].msg);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
